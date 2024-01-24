3ccb7f76d59749511c3dac37c334f9d9fe19b97c
Aswini-mca <aswini25.mca@gmail.com>
2024-01-24 09:24:42 +0530

    added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Home } from './components/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import About from './components/About';
-import Skills from './components/Skills';
 
 function App() {
   return (
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function Skills() {
-    const skillList =['HTML','CSS','Javascript','React','NodeJs','MongoDB','Bootstrap','MaterialUi']
   return (
     <div className='m-5'>
         <h2 className='container p-4'>My Skills</h2>
